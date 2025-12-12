044ee80f1e136df780fae15d99dae7d4e77ed5bc
Antoine Perras <2487106@bdeb.qc.ca>
2025-12-12 17:05:52 -0500

    Yasmine background anim fix

--- src/modules/page-principale/animDegradeBG.js
+++ src/modules/page-principale/animDegradeBG.js
@@ -3,7 +3,7 @@
 document.querySelector(".game-timeline").addEventListener('scroll', () => {
     const sections = document.querySelectorAll('.scroll-section');
     const body = document.body;
-
+ 
     sections.forEach(section => {
         const rect = section.getBoundingClientRect();
         if (rect.x <= window.innerWidth / 2) {
